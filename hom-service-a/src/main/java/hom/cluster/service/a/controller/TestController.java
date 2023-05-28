c387f8ee62c68d46f10e66d8a9d522727cf72d98
wangxh <wangxiaohong@kuihai.net>
2023-05-28 13:41:10 +0800

    网关白名单

--- hom-service-a/src/main/java/hom/cluster/service/a/controller/TestController.java
+++ hom-service-a/src/main/java/hom/cluster/service/a/controller/TestController.java
@@ -24,16 +24,24 @@ public class TestController {
     //2.接口暴露给外部（经网关转发）使用时：@NonAuth 或 @NonAuth(isInner = false)，但要在网关配置接口白名单
     //3.这个注解也可以添加到Controller上，将对其中的所有接口生效，且优先级高于添加到方法上
     @NonAuth(isInner = true)
-    @RequestMapping("/hello")
+    @RequestMapping("/hello") //这是一个供内部Feign调用的服务
     public Result hello(LoginUser loginUser){
         System.out.println("hello, loginUser: "+ JSON.toJSONString(loginUser));
         return Result.success("Hello, Im Service A, And test's value is: "+ testValue);
     }
 
+    //开放给外部的服务，需在网关中添加到白名单
     @NonAuth
     @RequestMapping("/open")
     public Result open(LoginUser loginUser){
         System.out.println("open, loginUser: "+ JSON.toJSONString(loginUser));
         return Result.success("Open, Im Service A");
+    }
+
+    //正常接口（需要登录的）
+    @RequestMapping("/haha")
+    public Result haha(LoginUser loginUser){
+        System.out.println("haha, loginUser: "+ JSON.toJSONString(loginUser));
+        return Result.success("Haha, Im Service A");
     }
 }
