46939359c364d54a6b708af6da117a9b5666fb76
wangxh <wangxiaohong@kuihai.net>
2023-05-28 15:08:25 +0800

    鉴权优化+

--- hom-common/hom-common-base/src/main/java/hom/cluster/common/base/constants/SecretKeyConst.java
+++ hom-common/hom-common-base/src/main/java/hom/cluster/common/base/constants/SecretKeyConst.java
@@ -9,9 +9,9 @@ public class SecretKeyConst {
     /**
      * 内部服务调用需要的密码（自定义）
      */
-    public static final String FEIGN_SECRET_KEY = "FEIGN_SECRET_KEY";
+    public static final String FEIGN_SECRET_KEY = "D6LDX6HvfhDRijEC%EaNfL!dhC%xPA29tGVUYNP^7aJM!5B&bsMDVge!JSi0ko0b";
     /**
      * 网关无登录时调用服务的密码（自定义）
      */
-    public static final String GATEWAY_SECRET_KEY = "GATEWAY_SECRET_KEY";
+    public static final String GATEWAY_SECRET_KEY = "ZJ#f6f%xOaeSexUyjbRYtiLU$yK4yQphNj$TfGH^c6X2j6YYsVM6f!rlOWy1w*@6";
 }
