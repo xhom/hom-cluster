770c7671167642119b4270a97ad8777ffe741147
wangxh <wangxiaohong@kuihai.net>
2023-05-28 12:48:11 +0800

    内部服务鉴权优化+

--- hom-open-feign/hom-feign-a/src/main/java/hom/cluster/feign/a/client/TestFeignClient.java
+++ hom-open-feign/hom-feign-a/src/main/java/hom/cluster/feign/a/client/TestFeignClient.java
@@ -1,6 +1,7 @@
 package hom.cluster.feign.a.client;
 
 import hom.cluster.common.base.res.Result;
+import hom.cluster.feign.a.config.MultipartSupportConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @description: 测试Feign客户端（对应一个Controller）
  * @date 2023/5/26 12:35
  */
-@FeignClient(value = "hom-service-a")
-//@FeignClient(value = "hom-service-a", configuration = MultipartSupportConfig.class)
+@FeignClient(value = "hom-service-a", configuration = MultipartSupportConfig.class)
 @RequestMapping("/test")
 public interface TestFeignClient {
     @RequestMapping("/hello")
