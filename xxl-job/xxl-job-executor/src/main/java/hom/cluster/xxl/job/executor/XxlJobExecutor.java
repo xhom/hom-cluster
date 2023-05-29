3e749aa199f311c30f9f569f87b528c3d3306405
wangxh <wangxiaohong@kuihai.net>
2023-05-29 17:33:58 +0800

    add xxl job executor

---
+++ xxl-job/xxl-job-executor/src/main/java/hom/cluster/xxl/job/executor/XxlJobExecutor.java
@@ -0,0 +1,11 @@
+package hom.cluster.xxl.job.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobExecutor {
+	public static void main(String[] args) {
+		SpringApplication.run(XxlJobExecutor.class, args);
+	}
+}
